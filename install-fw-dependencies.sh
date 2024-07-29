eb449ef633063b4d098c2f5a8e3d0c93ad8ff7b1
Lukas F. Hartmann <lukas@mntre.com>
2024-07-29 21:58:15 +0200

    install-fw-deps: that was too much recursion

--- install-fw-dependencies.sh
+++ install-fw-dependencies.sh
@@ -2,7 +2,11 @@
 
 sudo apt install build-essential pkg-config libusb-1.0-0-dev cmake gcc-arm-none-eabi tio
 
-git clone --depth 1 --recurse-submodules https://github.com/raspberrypi/pico-sdk
+git clone --depth 1 https://github.com/raspberrypi/pico-sdk
+cd pico-sdk
+git submodule init
+git submodule update --depth 1
+cd ..
 git clone --depth 1 https://github.com/raspberrypi/pico-extras
 git clone --depth 1 https://github.com/hathach/tinyusb
 
